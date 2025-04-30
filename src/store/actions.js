36bec65fd6d020e451b9e1b816e5863cf34c7db3
Isabel Campos <isacampos8a@gmail.com>
2025-04-29 23:03:59 -0500

    Search By Name complete

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,7 @@ export function searchMeals({ commit }, keyword) {
     api.get(`search.php?s=${keyword}`)
         .then(({ data }) => {
         commit('setSearchedMeals', data.meals)
-        })
+    })
 }
 
 export function searchMealsByLetter({ commit }, letter) {
@@ -18,7 +18,7 @@ export function searchMealsByLetter({ commit }, letter) {
 
 
 export function searchMealsByIngredient({ commit }, ingredient) {
-    api.get(`search.php?i=${ingredient}`)
+    api.get(`filter.php?i=${ingredient}`)
         .then(({ data }) => {
             commit('setMealsByIngredient', data.meals)
         })
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,4 @@
-export function getSearchedMeaks(state, meals) {
+export function setSearchedMeals(state, meals) {
     state.searchedMeals = meals || []
 }
 
